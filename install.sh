5da4403f07de767e1845816a0054e330a360c6ee
Justin Levy <justin@justlevy.com>
2023-02-03 19:36:32 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,51 +1,34 @@
 #!/bin/bash
 
 
-zshrc() {
-    create_symlinks() {
-        # Get the directory in which this script lives.
-        script_dir=$(dirname "$(readlink -f "$0")")
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
 
-        # Get a list of all files in this directory that start with a dot.
-        files=$(find -maxdepth 1 -type f -name ".*")
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
 
-        # Create a symbolic link to each file in the home directory.
-        for file in $files; do
-            name=$(basename $file)
-            echo "Creating symlink to $name in home directory."
-            rm -rf ~/$name
-            ln -s $script_dir/$name ~/$name
-        done
-    }
-
-    create_symlinks
-    
-
-
-    echo "==========================================================="
-    echo "             cloning zsh-autosuggestions                   "
-    echo "-----------------------------------------------------------"                    
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    echo "==========================================================="
-    echo "                 cloning zsh-syntax-highlighting           "
-    echo "-----------------------------------------------------------"                 
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    echo "             Installing fonts.                             "
-    FONT_DIR="$HOME/.fonts"
-    git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
-    cd $FONT_DIR
-    ./install.sh
-    echo "==========================================================="
-    
-    echo "==========================================================="
-    echo "             Setting up the Spaceship theme.               "
-    ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-    git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-    echo "==========================================================="
-    echo "                  Import zshrc                             "
-    echo "-----------------------------------------------------------"
-    cat .zshrc > $HOME/.zshrc
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
 }
 
-zshrc
+create_symlinks
+
+echo "Initializing conda for zsh."
+conda init zsh
+
+echo "Installing fonts."
+FONT_DIR="$HOME/.fonts"
+git clone https://github.com/powerline/fonts.git $FONT_DIR --depth=1
+cd $FONT_DIR
+./install.sh
+
+echo "Setting up the Spaceship theme."
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
